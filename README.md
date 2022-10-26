62e62f843c7e746d70019a271c0d5fd01d9e2b2f
liacalabaide <liana.calabaide@mercadolivre.com>
2022-10-26 09:44:29 -0300

    fix: optimize image size

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Exercicios FullCycle docker
 
 Dockerfile criado dentro da pasta main
-Tamanho final da imagem: 26.8MB
+Tamanho final da imagem: 1.2MB
+
+Para executar imagem:
+docker run -p 8080:8080 lianacalabaide/my-golang-scratch
--- main/main.go
+++ main/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Olá Mundo \n")
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	println("Code.education Rocks!")
 }
